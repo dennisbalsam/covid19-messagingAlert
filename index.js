bb3e1ee1faf392db302922da073cbf801df00bd6
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-04-16 13:43:34 -0400

    reverting time to 8 am

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ function sendOut(confirmed, recovered, deaths, date,time) {
 }
 
 
-cron.schedule("43 13 * * *", function() {
+cron.schedule("0 8 * * *", function() {
   console.log("cron function starting");
   fetchData().then((res)=> {
     //prepare values for send out
