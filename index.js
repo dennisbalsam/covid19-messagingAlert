655a54dfb1f00dd577541747f2af5b599a226664
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-04-16 03:35:03 -0400

    still debugging

--- index.js
+++ index.js
@@ -128,10 +128,11 @@ cron.schedule("* * * * *", function() {
 //create a basic post route for front end to store the user
 app.post('/api/data/', async (req, response) => {
 
-  response.setHeader('Access-Control-Allow-Origin', 'https://master.d3ej9f78ej6g5o.amplifyapp.com/');
+  response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
   response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
   response.setHeader('Access-Control-Allow-Credentials', true); // If needed
+  
   // create object for add
   let test  = {
     name: req.body.name,
