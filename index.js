8a88e5bea7b90ad063fb2ba864e4347155c9898c
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-04-16 03:01:49 -0400

    changing firebase toheroku env vars

--- index.js
+++ index.js
@@ -19,8 +19,16 @@ app.use(bodyParser.json())
 //firebase activation for heroku
 admin.initializeApp({
   credential: admin.credential.cert({
-   "private_key": process.env.FIREBASE_PRIVATE_KEY,
-   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "type": process.env.FIREBASE_TYPE,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
  }),
  databaseURL: "https://covid19-tracker-edc90.firebaseio.com"
 })
